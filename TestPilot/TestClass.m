c41928208175ade31eac1e340e844fde5f38ca77
Jon Crooke <joncrooke@gmail.com>
2013-02-08 16:37:20 +0100

    Builds, but streamline

--- TestPilot/TestClass.m
+++ TestPilot/TestClass.m
@@ -1,10 +1,13 @@
 //
-//  TestPilot.h
+//  TestClass.m
 //  TestPilot
 //
 //  Created by Jon Crooke on 08/02/2013.
 //  Copyright (c) 2013 joncrooke. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
-#import <OCMock/OCMock.h>
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
---
+++ Tests/Foundation_Tests.m
@@ -0,0 +1,22 @@
+//
+//  Foundation_Tests.m
+//  TestPilot
+//
+//  Created by Jon Crooke on 08/02/2013.
+//  Copyright (c) 2013 joncrooke. All rights reserved.
+//
+
+#import "TestPilot.h"
+
+@interface Foundation_Tests : SenTestCase
+@end
+
+@implementation Foundation_Tests
+
+- (void)testMocks
+{
+//  id mock = [OCMockObject niceMockForClass:[NSString class]];
+//  [mock verify];
+}
+
+@end
